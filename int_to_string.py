360b3fe62d3b5adfac349334dda0b9e4a72a5d9f
n-edwards <nicedwar@gmail.com>
2016-03-11 00:35:42 -0500

    scripts

---
+++ int_to_string.py
@@ -0,0 +1,9 @@
+myInteger = 47
+print (myInteger)          # 47
+print (type(myInteger))    # class is integer
+
+myString = str(myInteger)  # converting
+print (myString)           # 47 as string
+print (type(myString))     # class is string
+   
+
---
+++ int_to_string.rb
@@ -0,0 +1,7 @@
+myInteger = 47
+print myInteger, "\n"            # 47
+print myInteger.class, "\n"      # class is integer
+
+myString = myInteger.to_s  # converting
+print myString, "\n"             # 47 as string
+print myString.class, "\n"       # class is string
